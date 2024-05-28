99085cc47fd20460d4431ecfe434dd199d58ff91
jmbelmonte <jmbelmonte11@gmail.com>
2024-05-28 17:24:30 +0800

    Added Dashboard sample in Home page

--- WebApplication/HerWatch_WebApplication/Controllers/HomeController.cs
+++ WebApplication/HerWatch_WebApplication/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
 ﻿using HerWatch_WebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.VisualStudio.Web.CodeGeneration;
 
 namespace HerWatch_WebApplication.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
